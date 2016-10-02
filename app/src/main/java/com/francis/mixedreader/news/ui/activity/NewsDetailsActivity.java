95033bac442273bb4bc0cd392f6b1bf34b56748a
geniusvjr <geniusvjr@gmail.com>
2016-10-02 21:26:40 +0800

    引入 me.imid.swipebacklayout.lib:library:1.0.0

--- app/src/main/java/com/francis/mixedreader/news/ui/activity/NewsDetailsActivity.java
+++ app/src/main/java/com/francis/mixedreader/news/ui/activity/NewsDetailsActivity.java
@@ -3,7 +3,10 @@ package com.francis.mixedreader.news.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 import com.francis.mixedreader.model.NewsBean;
+import com.francis.mixedreader.news.presenter.NewsDetailsPresenter;
+import org.sufficientlysecure.htmltextview.HtmlTextView;
 
 /**
  * @author taoc @ Zhihu Inc.
@@ -16,6 +19,10 @@ import com.francis.mixedreader.model.NewsBean;
 public class NewsDetailsActivity extends AppCompatActivity{
 
 	private NewsBean mNewsBean;
+	private HtmlTextView mHtmlTextView;
+	private NewsDetailsPresenter mNewsDetailsPresenter;
+	private ProgressBar mProgressBar;
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
